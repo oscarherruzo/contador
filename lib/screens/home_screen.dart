ca8dfbe765fb97e92389d5872c4b7d5e1c0ab758
oscarherruzo <thelinkmeshx@gmail.com>
2022-10-07 13:54:44 +0200

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,48 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const miEstilo = TextStyle(fontSize: 20);
+    int contador = 0;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('MyApp'),
+        elevation: 10.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const <Widget>[
+            Text('Numero de clicks', style: miEstilo,),
+            Text('$contador', style: TextStyle(fontSize: 30),),
+          ],
+        ),
+    ),
+
+
+    floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterDocked,
+    floatingActionButton: FloatingActionButton(
+
+      child: Column(
+        children: const [
+          Icon(Icons.add ),
+          Icon(Icons.remove)
+        ],
+      ),
+      onPressed: () {
+        
+        print('Hola Mundo');
+        contador++;
+        },
+      ),
+    );
+  }
+
+}
